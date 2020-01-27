9519d0530880eda9f88b52f6bf7361a919e6a94d
Sota-Sugimura-1004 <sota.sugmiura01@g.softbank.co.jp>
2020-01-27 18:14:26 +0900

    Permission取得後、アプリが落ちる挙動の改良。

--- app/src/main/java/jp/techacademy/sota/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/sota/autoslideshowapp/MainActivity.kt
@@ -23,28 +23,16 @@ class MainActivity : AppCompatActivity() {
 
     private var mCursor: Cursor? = null
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
-            PERMISSIONS_REQUEST_CODE -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getFirstContentsInfo()
+                }else {
                     Log.d("ANDROID", "ACCESS DENIED")
                     finish()
                 }
-                return
-            }
-
-            // Add other 'when' lines to check for other
-            // permissions this app might request.
-            else -> {
-                // Ignore all other requests.
-            }
         }
     }
 
